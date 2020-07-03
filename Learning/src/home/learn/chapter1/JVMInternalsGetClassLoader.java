e230c7f342e7226ec23d76f0908e44bc6d6ca951
Shashank Sahu <shashanksahu11@gmail.com>
2020-07-03 20:11:13 +0530

    Source File rule

--- Learning/src/home/learn/chapter1/JVMInternalsGetClassLoader.java
+++ Learning/src/home/learn/chapter1/JVMInternalsGetClassLoader.java
@@ -6,7 +6,7 @@ public class JVMInternalsGetClassLoader
 	public static void main(String[] args) 
 	{ 
 		// String class is loaded by bootstrap loader, and 
-		// bootstrap loader is not Java object, hence null 
+		// bootstrap loader is not Java object, hence null
 		System.out.println(String.class.getClassLoader()); 
 
 		// Test class is loaded by Application loader 
---
+++ Learning/src/home/learn/chapter1/SourceFileNameRule.java
@@ -0,0 +1,33 @@
+package home.learn.chapter1;
+
+/**
+ * This is to show that a public class must be defined in
+ * its own file. Here class SourceFileNameRule is defined
+ * in SourceFileNameRule.java file.
+ * @author shashank
+ *
+ */
+public class SourceFileNameRule {
+
+	public static void main(String[] args) {
+		//Single line comment
+		/**
+		 * to generate code comment for class or method or 
+		 * to generate multiple line comment, just type /** and press 
+		 * enter.
+		 */
+
+	}
+
+}
+/**
+ * Non public class can be saved in file with any name
+ * Also class can only be public, abstract or final
+ * You can not make a top level class as private, although 
+ * you can make inner class as private.
+ * @author shashank
+ *
+ */
+ private class Test {
+	
+}
