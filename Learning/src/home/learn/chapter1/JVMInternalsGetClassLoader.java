a385cb8ccc4dd8b038df047d35fd9d2d30081a42
Shashank Sahu <shashanksahu11@gmail.com>
2020-07-03 18:58:01 +0530

    First commit from system

---
+++ Learning/src/home/learn/chapter1/JVMInternalsGetClassLoader.java
@@ -0,0 +1,15 @@
+package home.learn.chapter1;
+
+//Java code to demonstrate Class Loader subsystem 
+public class JVMInternalsGetClassLoader 
+{ 
+	public static void main(String[] args) 
+	{ 
+		// String class is loaded by bootstrap loader, and 
+		// bootstrap loader is not Java object, hence null 
+		System.out.println(String.class.getClassLoader()); 
+
+		// Test class is loaded by Application loader 
+		System.out.println(JVMInternalsGetClassLoader.class.getClassLoader()); 
+	} 
+}	 
